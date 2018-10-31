9e8c8f190e1e22cec77c20611e1768bf2c3f02b1
Evan <>
2018-10-31 21:06:38 +0100

    first commit
    Added title scene, map and coin/haddocks sprite. Proper project structuring.

---
+++ RefactoredRatvil/Assets/Scripts/Controllers/SceneSwitcher.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour
+{
+    public void GotoGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void GotoTitle()
+    {
+        SceneManager.LoadScene("Title");
+    }
+}

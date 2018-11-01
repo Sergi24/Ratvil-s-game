6e83cda68163b7f70d80ef0088e47d0ca19102c4
Evan <>
2018-11-01 20:09:42 +0100

    small improvements

---
+++ RefactoredRatvil/Assets/Scripts/Controllers/GameController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+
+    public void GotoGame(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
+    }
+
+    public void GotoTitle(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
+    }
+}
